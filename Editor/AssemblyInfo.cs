3e693dc70716fa836c42683286202460af24e7ea
KhanhTQ-Uni <khanhquang0912qh@gmail.com>
2025-03-07 13:39:24 +0700

    asset overwriter

---
+++ Editor/AssemblyInfo.cs
@@ -0,0 +1,4 @@
+using System.Reflection;
+
+[assembly: AssemblyTitle("com.ktgame.utils.asset-overwriter.editor")]
+[assembly: AssemblyProduct("com.ktgame.utils.asset-overwriter")]
---
+++ Runtime/AssemblyInfo.cs
@@ -0,0 +1,4 @@
+using System.Reflection;
+
+[assembly: AssemblyTitle("com.ktgame.utils.asset-overwriter")]
+[assembly: AssemblyProduct("com.ktgame.utils.asset-overwriter")]
